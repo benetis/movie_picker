38b36fe09bc2cdd4a18320dd37faad917faa3d14
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-07-24 18:55:25 +0300

    added todo list

---
+++ README.md
@@ -0,0 +1,5 @@
+## TODO 
+- [] Scrape X top movies with titles
+- [] Choose randomly X movies
+- [] Have an URL just to show the movie to watch
+- [] Have a DB where we can store some data and mark watched moveis
--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource movies!');
+  res.send('10 Random movies');
+});
+
+router.get('/scrape', function (req, res, next) {
+  res.send('Just scrapin the movies')
 });
 
 module.exports = router;
