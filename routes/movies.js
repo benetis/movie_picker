df012b2d896c59c0de09341e19012a3a15052eca
Zygimantas Benetis <zygimantasdev@gmail.com>
2016-07-24 19:47:03 +0300

    scraps movies' title in english

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var util = require('util');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('10 Random movies');
+  var url = 'http://www.imdb.com/search/title?groups=top_1000';
+  request(url, {headers: {'accept-language': 'en'}}, function (error, response, html) {
+    if(error) { return; }
+
+    var $ = cheerio.load(html);
+    var movies = [];
+
+    $('.detailed').filter(function () {
+      var movie = $(this).children();
+      var title = movie.last().find('a').first().text();
+      movies.push({title: title});
+    });
+    res.send(movies);
+  });
 });
 
 router.get('/scrape', function (req, res, next) {
